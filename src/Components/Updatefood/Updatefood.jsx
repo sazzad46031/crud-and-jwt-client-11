635427ed82917347f8e3c14d964f4ff357f63185
sazzad46031 <git config --global user.email sazzadmgbs@gmail.com>
2023-11-09 18:48:47 +0600

    private route is done

--- src/Components/Updatefood/Updatefood.jsx
+++ src/Components/Updatefood/Updatefood.jsx
@@ -1,15 +1,52 @@
 import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../Provider/AuthProvider";
+import Swal from "sweetalert2";
 
 
 const Updatefood = () => {
     const updatefood = useLoaderData()
     const {user} = useContext(AuthContext)
+
+    const handleUpdateFood = event => {
+        event.preventDefault()
+        const form = event.target
+        const foodName =form.foodName.value
+        const foodImage =form.foodImage.value
+        const foodQuantity =form.foodQuantity.value
+        const pickupLocation =form.pickupLocation.value
+        const expiredDate =form.expiredDate.value
+        const additionalNotes =form.additionalNotes.value
+        const foodStatus =form.foodStatus.value
+        const donatorName =form.donatorName.value 
+        const donatorEmail =form.donatorEmail.value
+        const donatorImage =form.donatorImage.value
+        const updateFood = {foodName, foodImage, foodQuantity, pickupLocation, expiredDate, additionalNotes, foodStatus, donatorName, donatorEmail, donatorImage}
+        console.log(updateFood)
+        fetch(`http://localhost:5000/foods/updatefood/${_id}`,{
+            method: 'PUT',
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify(updateFood)
+        })
+        .then(res=> res.json())
+        .then(data=>{
+            console.log(data)
+            if(data.insertedId){
+                Swal.fire({
+                    title: 'success!',
+                    text: 'Food updated successfully',
+                    icon: 'success',
+                    confirmButtonText: 'Cool'
+                  })
+            }
+        })
+    }
     return (
         <div className="bg-lime-200 max-w-screen-2xl mx-auto p-24">
         <h2 className="text-5xl text-purple-400 font-bold mb-8 text-center">Update Food</h2>
-        <form>
+        <form onSubmit={handleUpdateFood}>
             <div className="md:flex mb-8">
                 <div className="form-control md:w-1/2">
                     <label className="label">
